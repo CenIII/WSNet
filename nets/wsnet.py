cd60d2b9da41b015db532e3432ea3a4d3a538a4d
Chuan Cen <chuancen@deeplearn3.eecs.umich.edu>
2019-07-14 04:03:58 -0400

    reformat code.

--- nets/wsnet.py
+++ nets/wsnet.py
@@ -16,41 +16,41 @@ class WeaklySupNet(nn.Module):
     """
     An end-to-end semantic segmentation net via cross-entropy loss only. 
     """
-    def __init__(self,nclass):
-        super(WeaklySupNet,self).__init__()     
+    def __init__(self, nclass):
+        super(WeaklySupNet, self).__init__()     
         kq_dim = 16
         self.backbone = nn.Sequential(
-            nn.Conv2d(3, 32, (5, 5)),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(3),
-            nn.Conv2d(32, 64, (3, 3)),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 64, (3, 3)),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(3, 32, (5, 5)), 
+            nn.BatchNorm2d(32), 
+            nn.ReLU(), 
+            nn.MaxPool2d(3), 
+            nn.Conv2d(32, 64, (3, 3)), 
+            nn.BatchNorm2d(64), 
+            nn.ReLU(), 
+            nn.MaxPool2d(2), 
+            nn.Conv2d(64, 64, (3, 3)), 
+            nn.BatchNorm2d(64), 
             nn.ReLU()
             )
-        self.kq = KQ(64,kq_dim)
+        self.kq = KQ(64, kq_dim)
         self.feature = nn.Sequential(
-            nn.Conv2d(64, 64, (3,3),padding=1),
+            nn.Conv2d(64, 64, (3, 3), padding=1), 
             nn.ReLU()
         )
-        self.bayes = Bayes(64,kq_dim,2,n_heads=1,dif_pattern=[(3,6)],rel_pattern=[(5,3)])
-        self.gap = Gap(64,nclass)
+        self.bayes = Bayes(64, kq_dim, 2, n_heads=1, dif_pattern=[(3, 6)], rel_pattern=[(5, 3)])
+        self.gap = Gap(64, nclass)
         self.nclass = nclass
 
-    def getHeatmaps(self,classid):
-        zzz = classid[:,None,None,None].repeat(1,self.gap.heatmaps.shape[1],self.gap.heatmaps.shape[2],1)
-        hm = torch.gather(self.gap.heatmaps,3,zzz).squeeze()
+    def getHeatmaps(self, classid):
+        zzz = classid[:, None, None, None].repeat(1, self.gap.heatmaps.shape[1], self.gap.heatmaps.shape[2], 1)
+        hm = torch.gather(self.gap.heatmaps, 3, zzz).squeeze()
         return hm
 
-    def forward(self,x,label):
+    def forward(self, x, label):
         bb = self.backbone(x)
-        K,Q = self.kq(bb)
+        K, Q = self.kq(bb)
         feats = self.feature(bb)
-        feats1, preds1 = self.bayes(feats,K,Q,label)
-        pred = self.gap(feats1,save_hm=True)
+        feats1, preds1 = self.bayes(feats, K, Q, label)
+        pred = self.gap(feats1, save_hm=True)
         return preds1, pred
     
--- train_wsn_small.py
+++ train_wsn_small.py
@@ -28,7 +28,7 @@ def visualize(net, label, fig, ax, cb, iterno):
 		img = ax[0][i].imshow(hm[1+i*4])
 		if cb[0][i] is not None:
 			cb[0][i].remove()
-		cb[0][i] = plt.colorbar(img,ax=ax[0][i])
+		cb[0][i] = plt.colorbar(img, ax=ax[0][i])
 
 	# plot here
 	# for i in range(len(ax[1])):
@@ -36,7 +36,7 @@ def visualize(net, label, fig, ax, cb, iterno):
 	# 	img = ax[1][i].imshow(hm)
 	# 	if cb[1][i] is not None:
 	# 		cb[1][i].remove()
-	# 	cb[1][i] = plt.colorbar(img,ax=ax[1][i])
+	# 	cb[1][i] = plt.colorbar(img, ax=ax[1][i])
 	
 
 	# hm = net.getRelHm(label).data.cpu().numpy()
@@ -44,15 +44,15 @@ def visualize(net, label, fig, ax, cb, iterno):
 	# 	img = ax[2][i].imshow(hm[1+i*4])
 	# 	if cb[2][i] is not None:
 	# 		cb[2][i].remove()
-	# 	cb[2][i] = plt.colorbar(img,ax=ax[2][i])
+	# 	cb[2][i] = plt.colorbar(img, ax=ax[2][i])
 	
 	# get 8th image's both heatmaps
 	# hm = net.heatmaps[8].data.cpu().numpy()
 	# for i in range(len(ax[2])):
-	# 	img = ax[2][i].imshow(hm[:,:,i+1])
+	# 	img = ax[2][i].imshow(hm[:, :, i+1])
 	# 	if cb[2][i] is not None:
 	# 		cb[2][i].remove()
-	# 	cb[2][i] = plt.colorbar(img,ax=ax[2][i])
+	# 	cb[2][i] = plt.colorbar(img, ax=ax[2][i])
 
 	fig.suptitle('iteration '+str(iterno))
 	plt.pause(0.05)
@@ -62,9 +62,9 @@ def loadData():
 	filelist = sorted(os.listdir('./data'))
 	imgs = []
 	for file in filelist:
-		imgs.append(np.moveaxis(cv2.imread(os.path.join('./data/',file)),-1,0))
-	label = [[1,0],[1,0],[1,0],[1,0],[0,1],[0,1],[0,1],[0,1]]
-	label_vis = [0,0,0,0,1,1,1,1]
+		imgs.append(np.moveaxis(cv2.imread(os.path.join('./data/', file)), -1, 0))
+	label = [[1, 0], [1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1], [0, 1]]
+	label_vis = [0, 0, 0, 0, 1, 1, 1, 1]
 	imgs = torch.tensor(imgs).type(device.FloatTensor)
 	label = torch.tensor(label).type(device.FloatTensor)
 	label_vis = torch.tensor(label_vis)#.type(device.FloatTensor)
@@ -73,16 +73,16 @@ def loadData():
 
 def gauss_filt(data): #[8, 3, 512, 512]
 	# mask
-	zzz=data.sum(1,True)
+	zzz=data.sum(1, True)
 	zzz[zzz>0]=1
-	mask = 1-zzz.repeat(1,3,1,1)
+	mask = 1-zzz.repeat(1, 3, 1, 1)
 	# generate gaussian noise
-	N,C,H,W = data.shape
+	N, C, H, W = data.shape
 	mean = 128
 	var = 2000
 	sigma = var**0.5
-	gauss = np.clip(np.around(np.random.normal(mean,sigma,(int(N/2),C,1,1)),decimals=0),0,255)
-	gauss = torch.from_numpy(gauss).type(device.FloatTensor).repeat(2,1,H,W)
+	gauss = np.clip(np.around(np.random.normal(mean, sigma, (int(N/2), C, 1, 1)), decimals=0), 0, 255)
+	gauss = torch.from_numpy(gauss).type(device.FloatTensor).repeat(2, 1, H, W)
 	# apply gaussian noise
 	ret = data + gauss * mask
 	ret = (ret-128.)/255.
@@ -101,13 +101,13 @@ def train(net, data, label, label_vis, optimizer, crit0, epoches=100):
 	fig, ax = plt.subplots(nrows=3, ncols=2)
 	
 	iterno = 0
-	cb = [[None,None],[None,None],[None,None]]
+	cb = [[None, None], [None, None], [None, None]]
 	filt_data = [gauss_filt(data) for i in range(5)]
 	while True:
 		if iterno%1==0:
-			indlist = np.random.choice(5,5,replace=False)
+			indlist = np.random.choice(5, 5, replace=False)
 		idx = np.arange(8)
-		preds, pred0 = net(filt_data[indlist[iterno%5]][idx],label_vis[idx]) #,pred_tmask
+		preds, pred0 = net(filt_data[indlist[iterno%5]][idx], label_vis[idx]) #, pred_tmask
 		loss = []
 		for pred in preds:
 			tmp = crit0(pred, label[idx])
@@ -119,7 +119,7 @@ def train(net, data, label, label_vis, optimizer, crit0, epoches=100):
 		print('iterno='+str(iterno)+', loss='+str(loss))
 		optimizer.step()
 		if iterno%5==0:
-			visualize(net,label_vis[idx],fig,ax,cb,iterno)
+			visualize(net, label_vis[idx], fig, ax, cb, iterno)
 		iterno += 1
 
 
@@ -127,7 +127,7 @@ def train(net, data, label, label_vis, optimizer, crit0, epoches=100):
 if __name__ == '__main__':
 
 	net = WeaklySupNet(nclass=2)
-	optimizer = torch.optim.Adam(net.parameters(),lr=0.001)
+	optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 	crit0 = torch.nn.MultiLabelSoftMarginLoss()
 	data, label, label_vis = loadData()
 	train(net, data, label, label_vis, optimizer, crit0)
