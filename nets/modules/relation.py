468c6ee34f68c8cffc4def85e618042a092416f5
Chuan Cen <chuancen@deeplearn10.eecs.umich.edu>
2019-07-22 22:26:02 -0400

    ...

--- nets/modules/relation.py
+++ nets/modules/relation.py
@@ -50,12 +50,12 @@ class Boundary(nn.Module):
     def __init__(self,in_channels):
         super(Boundary,self).__init__()
         self.nn = nn.Sequential(
-            nn.Conv2d(in_channels,in_channels,(3,3),padding=1),
+            nn.Conv2d(in_channels,in_channels,(3,3)),#,padding=1),
             nn.GroupNorm(4,in_channels),
             nn.ReLU(),
-            # nn.Conv2d(in_channels,in_channels,(3,3),padding=1),
-            # nn.GroupNorm(8, in_channels),
-            # nn.ReLU(),
+            nn.Conv2d(in_channels,in_channels,(1,1),padding=1),
+            nn.GroupNorm(4, in_channels),
+            nn.ReLU(),
             # nn.Conv2d(in_channels,in_channels,(3,3),padding=1),
             # nn.GroupNorm(8, in_channels),
             # nn.ReLU(),
@@ -112,7 +112,7 @@ class Infusion(nn.Module):
         boundary_sim = torch.ones_like(boundary_max)-boundary_max
         boundary_sim = boundary_sim.unsqueeze(0).unsqueeze(0) # torch.Size([1, 1, 25, 52488])
         
-        att =  boundary_sim/torch.clamp(torch.sum(boundary_sim,dim=2,keepdim=True),1) #torch.softmax(boundary_sim, 2) # [1,1,fW*fH,W*H*B]
+        att =  boundary_sim/torch.clamp(torch.sum(boundary_sim,dim=2,keepdim=True),0.1) #torch.softmax(boundary_sim, 2) # [1,1,fW*fH,W*H*B]
         
         V_trans = im2col_indices(V, Hf, Wf, padding, 1, dilation).view(1, self.v_dim, Hf*Wf, -1) # torch.Size([1, 2, 9, 52488])
         out = (V_trans * att).sum(2).sum(0).view(D, H, W, N).permute(3, 0, 1, 2)/self.n_heads # torch.Size([8, 2, 81, 81])
--- nets/wsnet.py
+++ nets/wsnet.py
@@ -19,27 +19,30 @@ class WeaklySupNet(nn.Module):
     def __init__(self, nclass):
         super(WeaklySupNet, self).__init__()     
         kq_dim = 16
-        self.backbone = nn.Sequential(
-            nn.Conv2d(3, 32, (5, 5)), 
+        self.backbone = nn.ModuleList([nn.Sequential(
+            nn.Conv2d(3, 32, (5, 5), padding=2), 
             nn.BatchNorm2d(32), 
             nn.ReLU(), 
             nn.MaxPool2d(3), 
-            nn.Conv2d(32, 64, (3, 3)), 
+        ), nn.Sequential(
+            nn.Conv2d(32, 64, (3, 3), padding=1), 
             nn.BatchNorm2d(64), 
             nn.ReLU(), 
             nn.MaxPool2d(2), 
-            nn.Conv2d(64, 64, (3, 3)), 
+        ), nn.Sequential(
+            nn.Conv2d(64, 64, (3, 3), padding=1), 
             nn.BatchNorm2d(64), 
             nn.ReLU()
             )
-        self.kq = KQ(64, kq_dim)
-        self.boundary = Boundary(64)
+        ])
+        self.parymid = nn.MaxPool2d(2)
+        
+        self.boundary = Boundary(160)
         self.branch_local = nn.Sequential(
-            nn.Conv2d(64, 64, (3, 3), padding=1)
+            nn.Conv2d(64, 64, (3, 3), padding=1),
+            nn.BatchNorm2d(64),
         )
-        self.branch_relation = nn.Sequential(
-            nn.Conv2d(64, 64, (3, 3), padding=1)
-        )
+        
         self.gap0 = Gap(64, nclass)
         # self.relation = Relation(2, kq_dim, 2, n_heads=1, rel_pattern=[(3,3),(3,1),(5,3),(5,5)])
         self.relation = Relation(2, kq_dim, 2, n_heads=1, rel_pattern=[(3,3),(3,1),(3,5),(5,1)])
@@ -57,18 +60,20 @@ class WeaklySupNet(nn.Module):
         return hm
 
     def forward(self, x, label):
-        bb = self.backbone(x)
+        x1 = self.backbone[0](x)
+        x2 = self.backbone[1](x1)
+        bb = self.backbone[2](x2)
+        feats_rel = torch.cat((self.parymid(x1),x2,bb),dim=1)
         feats_lc = self.branch_local(bb)
-        feats_rel = self.branch_relation(bb)
-        # import pdb;pdb.set_trace()
-        # K, Q = self.kq(feats_rel)
-        boundary = self.boundary(bb)
+        
+        boundary = self.boundary(feats_rel)
         pred0, cam0 = self.gap0(feats_lc)
         pred1, cam1 = self.relation(cam0, boundary)
         pred2, cam2 = self.relation(cam1, boundary)
+        pred3, cam3 = self.relation(cam2, boundary)
 
         self.bmap = boundary.squeeze()
         self.initheatmaps = cam0.permute(0,2,3,1)
-        self.heatmaps = cam2.permute(0,2,3,1)
+        self.heatmaps = cam3.permute(0,2,3,1)
     
-        return [pred1, pred2], pred0 
+        return [pred1, pred2, pred3], pred0 
